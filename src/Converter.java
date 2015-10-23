662d0a5a8b96653e5751b9d599c6021236337972
Tim Killenberger <timkillenberger@gmail.com>
2015-10-23 09:00:37 +0200

    Rework

--- src/Converter.java
+++ src/Converter.java
@@ -7,31 +7,36 @@ import java.util.ArrayList;
 
 public class Converter {
 
-	static String decoded = "";
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BigInteger bytes;
-	static BigInteger divider = new BigInteger("9");
-	static BigInteger multiplier = new BigInteger("-2");
-	static String input = "";
-
+	public static String decoded = "";
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static BigInteger bytes;
+	private static BigInteger divider = new BigInteger("9");
+	private static BigInteger multiplier = new BigInteger("-2");
+	protected static String input = "";
 	
 	// Encrypts a given input (String and numbers possible)
-	public static void encrypt() throws IOException {
+	protected static void encrypt() throws IOException {
 
 		String cacheBytes = "";
 
-		System.out.println("Geben sie den zu verschlÃ¼sselnden Text ein: ");
-		decoded = br.readLine(); // saves the input String 
+		System.out.println("Geben sie den zu verschl\u00fcsselnden Text ein: ");
 
+		decoded = br.readLine(); // saves the input String 
+		decoded.replaceAll("ü", "\u00fc");
 		// if input is not empty programm starts
 		if (!decoded.isEmpty()) {
 			byte[] b = decoded.getBytes(StandardCharsets.UTF_8); // creates a byteArray with UTF-8 Encoding
 
 			for (int i = 0; i < b.length; i++) {
-				cacheBytes += Byte.toString(b[i]) + 14; // converts every single char into bytes and adds puts them together
+				cacheBytes += Byte.toString(b[i]) + 23; // converts every single char into bytes and adds puts them together
 			}
-
-			bytes = new BigInteger(cacheBytes); // byteString gets convertet into a number
+			
+			try {
+				bytes = new BigInteger(cacheBytes); // byteString gets convertet into a number
+			} catch(NumberFormatException e) {
+				System.out.println("Die eingabe ist ungültig! lel \n");
+				return;
+			}
 			bytes = bytes.divide(divider); // bytes are getting divided
 			bytes = bytes.multiply(multiplier); // bytes are getting multiplied
 
@@ -42,47 +47,68 @@ public class Converter {
 	}
 
 	// decrypts a given Tim64 number
-	public static void decrypt() throws IOException {
+	private static void decrypt() throws IOException {
 		String cacheString = "";
 		String[] byteCode = {};
 		ArrayList<Byte> singleBytes = new ArrayList<Byte>();
 		byte[] singleBytes2;
 
-		System.out.println("Geben sie die zu entschlÃ¼sselnden Zahlen ein: ");
+		System.out.println("Geben sie die zu entschl\u00fcsselnden Zahlen ein: ");
 		decoded = br.readLine(); // saves input
-
+		
 		// if String is not empty program starts
 		if(!decoded.isEmpty()) {
+			try {
+				bytes = new BigInteger(decoded); // Tim64 bytes are getting converted into numbers
+			} catch(NumberFormatException e) {
+				System.out.println("Die eingabe ist ungültig! \n");
+				return;
+			}
+			bytes = bytes.divide(multiplier); // bytes are multiplied
+			bytes = bytes.multiply(divider); // bytes are divided
 
-		bytes = new BigInteger(decoded); // Tim64 bytes are getting converted into numbers
-		bytes = bytes.divide(multiplier); // bytes are multiplied
-		bytes = bytes.multiply(divider); // bytes are divided
-
-		cacheString = bytes.toString(); // bytes are converted into String
-
-		String leer = cacheString.replace("14", " "); // "14" is replaced with " "
-		byteCode = leer.split(" "); // byteString is splitted on every " "
-
-		for (String s : byteCode) {
-			singleBytes.add((byte) Integer.parseInt(s)); // every string in StringArray is added to Byte Arraylist 
-		}
-		singleBytes2 = new byte[singleBytes.size()]; // new byteArray is initialized
-
-		for (int i = 0; i < singleBytes.size(); i++) {
-			singleBytes2[i] = singleBytes.get(i); // every byte in Byte ArrayList is transfered into byteArray
-		}
-
-		String decoded = new String(singleBytes2, "UTF-8"); // the bytes in the byteArray are converted into a String
-		System.out.println("DECODED: " + decoded);
+			cacheString = bytes.toString(); // bytes are converted into String
+	
+			String leer = cacheString.replace("23", " "); // "23" is replaced with " "
+			byteCode = leer.split(" "); // byteString is splitted on every " "
+	
+			for (String s : byteCode) {
+				if(!s.equals(null) && !s.equals(" ") && !s.equals("")) {
+					singleBytes.add((byte) Integer.parseInt(s)); // every string in StringArray is added to Byte Arraylist 
+				}
+			}
+			singleBytes2 = new byte[singleBytes.size()]; // new byteArray is initialized
+	
+			for (int i = 0; i < singleBytes.size(); i++) {
+				singleBytes2[i] = singleBytes.get(i); // every byte in Byte ArrayList is transfered into byteArray
+			}
+	
+			String decoded = new String(singleBytes2, "UTF-8"); // the bytes in the byteArray are converted into a String
+			System.out.println("DECODED: " + decoded);
 		} else {
 			new Encryptor();
 		}
 	}
 	
-	public static void readInput() throws IOException {
-		System.out.println("e fÃ¼r encrypt / d fÃ¼r decrypt / exit zum beenden");
-		input = br.readLine();
-
+	protected static void readInput() throws IOException {
+		System.out.println("e f\u00fcr encrypt / d f\u00fcr decrypt / exit zum beenden");
+		checkinput(input = br.readLine());
 	}
 	
+	private static void checkinput(String input) throws IOException {
+		
+		if (Converter.input.toLowerCase().equals("e")) {
+			encrypt();
+		} else if (Converter.input.toLowerCase().equals("d")) {
+			decrypt();
+		} else if (Converter.input.toLowerCase().equals("exit")) {
+			System.out.println("Tim64 is shutting down...");
+			System.out.println("Successful!");
+			System.exit(0);
+		} else {
+			System.out.println("Eingabe war falsch bitte erneut eingeben!");
+			return;
+		}
+		System.out.println("Wollen sie erneut eine Eingabe machen?");
+	}
 }
--- src/Encryptor.java
+++ src/Encryptor.java
@@ -6,16 +6,6 @@ public class Encryptor {
 
 		while (Converter.input != "e" || Converter.input != "d" || Converter.input != "exit") {
 			Converter.readInput();
-
-			if (Converter.input.toLowerCase().equals("e")) {
-				Converter.encrypt();
-			} else if (Converter.input.toLowerCase().equals("d")) {
-				Converter.decrypt();
-			} else if (Converter.input.toLowerCase().equals("exit")) {
-				System.exit(0);
-			}
-			
-			System.out.println("Eingabe war falsch! Bitte eine richtige Eingabe machen.");
 		}
 	}
 }
